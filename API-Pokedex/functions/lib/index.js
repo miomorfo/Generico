0200e09b53671ede60c0b24b1cf3fabcc4dbcf9f
Emmanuel Ortiz Matus <eortizma@everis.com>
2020-03-13 17:29:51 -0300

    add new function
    se agrega funcion donde se consulta todos los elementos de la coleccion

--- API-Pokedex/functions/lib/index.js
+++ API-Pokedex/functions/lib/index.js
@@ -1,12 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const admin = require("firebase-admin");
+const serviceAccount = require("./servicesAccountKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://pokedex-generica.firebaseio.com"
+});
+const db = admin.firestore();
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send({
         mensaje: 'Hello Pokedex-Generica from Firebase Hosting!!!'
     });
+});
+exports.GetPokemon = functions.https.onRequest(async (request, response) => {
+    const pokeRef = db.collection('pokedex-kanto');
+    const docsSanp = await pokeRef.get();
+    const pokeAll = docsSanp.docs.map(doc => doc.data());
+    response.json(pokeAll);
 });
 //# sourceMappingURL=index.js.map
--- API-Pokedex/functions/src/index.ts
+++ API-Pokedex/functions/src/index.ts
@@ -1,10 +1,29 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const  serviceAccount = require("./servicesAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://pokedex-generica.firebaseio.com"
+  });
+
+const db = admin.firestore();
+
+
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send({
      mensaje: 'Hello Pokedex-Generica from Firebase Hosting!!!'
  });
+});
+
+
+export const GetPokemon = functions.https.onRequest( async(request, response) => {
+
+const pokeRef = db.collection('pokedex-kanto');
+const docsSanp = await pokeRef.get();
+
+const pokeAll = docsSanp.docs.map( doc => doc.data())
+
+response.json(pokeAll);
 });
