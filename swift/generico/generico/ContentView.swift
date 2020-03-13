709df92ada7554605d73bc782030ac01d3e4433d
Fernando <fernando.eb@icloud.com>
2020-03-13 17:19:47 -0300

    inicio app swift

---
+++ swift/generico/generico/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  generico
+//
+//  Created by Fernando on 13-03-20.
+//  Copyright © 2020 copycat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Pokemon")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
