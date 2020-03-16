7faef2172e9d2a9ba81d355306ecf9b4fe3fd178
Emmanuel Ortiz Matus <eortizma@everis.com>
2020-03-16 16:47:16 -0300

    inicio app kotlin

---
+++ Kotlin/app/src/main/java/com/example/generico/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.generico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
